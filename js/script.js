68f0bee02cfeba6ccfa04094e42db015085a7e5d
abdumutalovm <mirziyodabdumutalov83@gmail.com>
2024-01-12 23:07:01 +0500

    4-month 1-lesson

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Homerwork
+
+// ex-1
+const cub = 3;
+let volume = cub * cub * cub;
+console.log(volume);
+
+// ex-2
+let cub1 = 3;
+const volume1 = cub1 * cub1 * cub1;
+console.log(volume);
+
+// ex-3
+let a = 10;
+let b = 6;
+let result = a + b;
+result = result * result;
+console.log(result);
+
+// ex-4
+let optionlarNumber = 154;
+let result1 = (optionlarNumber / 10) % 10;
+console.log(result1);
+
+// ex-5
+let optionarNumber1 = 54;
+let firstNumber = optionarNumber1 % 10;
+let secondNumber = optionlarNumber / 10;
+let result2 = firstNumber + secondNumber;
+console.log(result2);
